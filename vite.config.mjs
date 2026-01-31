8f67496b880550f9e977868ad845d03e15967b00
tamerlanogo-art <tamerlanogo@gmail.com>
2026-01-31 10:31:07 +0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,4 @@
-import tailwindcss from "@tailwindcss/vite";
+gimport tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
